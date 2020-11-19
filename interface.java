006edfbdc8efb3ccfa5734cc76e86a7a3ab6c186
Fabricio <fabricioz.1.2@hotmail.com>
2020-11-19 00:37:34 -0300

    add interface

---
+++ interface.java
@@ -0,0 +1,8 @@
+/*interface é um elemento em que todos os metodos dentro dela nao tem codigo e sao abstract 
+AULA - classe abstrata e interface 
+
+Pode-se extender apenas uma casse, mas implementar quantas eu quiser
+
+classe abstrata - passar adiatnte funcionalidades e responsabiidades, tenho algo e quero q voce faca o resto (exemplo do agente na arena);
+interface- quando nnao tenho nada ou nao quero ter nada e precis garantir um padrao, passo apenas responsabilidades e regras; 
+*/
